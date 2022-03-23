5dff65cf0d25a446d28d2eb49d33000e84bd9962
Skippy-the-derg <98895095+Skippy-the-derg@users.noreply.github.com>
2022-03-23 12:54:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ function format(command, value) {
     document.execCommand('insertHTML', false, '<a href="' + url + '" target="_blank">' + sText + '</a>');
         //format('createlink', url);
  }
+<div class="sample-toolbar">
+<span class="fa fa-bold fa-fw" aria-hidden="true"></span>
+<span class="fa fa-italic fa-fw" aria-hidden="true"></span>
+<span class="fa fa-list fa-fw" aria-hidden="true"></span>
+<span class="fa fa-link fa-fw" aria-hidden="true"></span>
+<span class="fa fa-bold fa-fw" onclick="format('bold')"></span>
+<span class="fa fa-italic fa-fw" onclick="format('italic')"></span>
+<span class="fa fa-list fa-fw" onclick="format('insertunorderedlist')"></span>
+</div>
